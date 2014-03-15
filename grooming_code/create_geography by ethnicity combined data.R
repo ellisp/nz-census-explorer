0f58795333ae3a21f3f4bab249bbdb5ec66ae19d
ellisp <peter.ellis2013nz@gmail.com>
2014-03-16 09:53:40 +1300

    four working variables

--- grooming_code/create_geography by ethnicity combined data.R
+++ grooming_code/create_geography by ethnicity combined data.R
@@ -9,7 +9,7 @@ combined_cast_tabs <- list()
 #================create a range of combined variables
 
 
-#-----------Income_T4_all---------------
+#-----------2 variables from Income_T4_all---------------
 Income_T4_all <- rbind(C01Income_T4, C06Income_T4, Cincome_T4)
 Income_T4_all$Ethnicity <- rename.levels(Income_T4_all$Ethnicity, orig="Mäori", new="Maori")
 
@@ -18,22 +18,44 @@ combined_cast_tabs[[1]] <- dcast(Income_T4_all, Geography_Type + Geography + Yea
                          function(x){mean(x, na.rm=TRUE)}, 
                          value.var = target)
 combined_cast_tabs[[1]]$variable <- target
+names(combined_cast_tabs[[1]])[names(combined_cast_tabs[[1]])==
+                                 "Total Responding with at least one ethnicity"] <-"Total responding with at least one ethnicity"
+  
 
 target <- "Total_People"
 combined_cast_tabs[[2]] <- dcast(Income_T4_all, Geography_Type + Geography + Year ~ Ethnicity, 
                                  function(x){mean(x, na.rm=TRUE)}, 
                                  value.var = target)
 combined_cast_tabs[[2]]$variable <- target
+names(combined_cast_tabs[[2]])[names(combined_cast_tabs[[2]])==
+                                 "Total Responding with at least one ethnicity"] <-"Total responding with at least one ethnicity"
+
+
+#--------------------2 from the Dwell_HomeT2B-------------------------------
+
+Rental_T2_all <- rbind(C01Dwell_HomeT2b, C06Dwell_HomeT2b, CDwell_Home_T2b)
+names(Rental_T2_all)[names(Rental_T2_all) == "Ethnicity_of_Reference_Person"] <- "Ethnicity"
+
+Rental_T2_all$Ethnicity <- rename.levels(Rental_T2_all$Ethnicity, orig="Mäori", new="Maori")
 
 
 
+target <-"Median_Rent_Dollars"
+combined_cast_tabs[[3]] <- dcast(Rental_T2_all, Geography_Type + Geography + Year ~ Ethnicity, 
+                                 function(x){mean(x, na.rm=TRUE)}, 
+                                 value.var = target)
+combined_cast_tabs[[3]]$variable <- target
 
-
+target <- "Total_Rented_Private_Occupied_Dwellings"
+combined_cast_tabs[[4]] <- dcast(Rental_T2_all, Geography_Type + Geography + Year ~ Ethnicity, 
+                                 function(x){mean(x, na.rm=TRUE)}, 
+                                 value.var = target)
+combined_cast_tabs[[4]]$variable <- target
 
 #==============processing of census_combined dataset====================
 
-combined_cast_tabs <- do.call("rbind", combined_cast_tabs)
-census_combined <- subset(combined_cast_tabs, Geography_Type %in% c("TA", "Region"))
+combined_cast_tabs2 <- do.call("rbind", combined_cast_tabs)
+census_combined <- subset(combined_cast_tabs2, Geography_Type %in% c("TA", "Region"))
 
 names(census_combined)[names(census_combined) == "Geography"] <- "NAME"
 census_combined <- census_combined[ -grep("Area Outside", census_combined$NAME), ]
