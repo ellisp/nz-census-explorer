b3dfd4f7c3245c6c79bce6a00ddcba815e763c78
ellisp <peter.ellis2013nz@gmail.com>
2014-03-16 08:52:02 +1300

    tidy up processing so things in right order to later on add variables

--- grooming_code/create_TA_household_ethnicity_income.R
+++ grooming_code/create_TA_household_ethnicity_income.R
@@ -9,25 +9,39 @@ Cincome_T4$Year <- 2013
 Income_T4_all <- rbind(C01Income_T4, C06Income_T4, Cincome_T4)
 
 Income_T4_all$Ethnicity <- rename.levels(Income_T4_all$Ethnicity, orig="Mäori", new="Maori")
+# Income_T4_all$Variable <- 
 
-census_combined <- subset(Income_T4_all, Geography_Type %in% c("TA", "Region"))
-census_combined <- census_combined[ -grep("Area Outside", census_combined$Geography), ]
 
-nc <- nchar(as.character(census_combined$Geography))
-census_combined$NAME <- with(census_combined, gsub(" District", "", substring(Geography, 
+
+Income_T4_all_c <- dcast(Income_T4_all, Geography_Type + Geography + Year ~ Ethnicity, 
+                         function(x){mean(x, na.rm=TRUE)}, 
+                         value.var = "Median_Household_Income_Dollars")
+
+
+
+
+
+
+
+
+#==============processing of census_combined
+census_combined <- subset(Income_T4_all_c, Geography_Type %in% c("TA", "Region"))
+
+names(census_combined)[names(census_combined) == "Geography"] <- "NAME"
+census_combined <- census_combined[ -grep("Area Outside", census_combined$NAME), ]
+
+nc <- nchar(as.character(census_combined$NAME))
+census_combined$NAME <- with(census_combined, gsub(" District", "", substring(NAME, 
                                                         ifelse(Geography_Type == "TA", 5, 4)
                                                                               , nc)))
 census_combined$NAME <- gsub(" City", "", census_combined$NAME)
 census_combined$NAME <- gsub(" Region", "", census_combined$NAME)
 
-
-census_combined <- dcast(census_combined, Geography_Type + NAME + Year ~ Ethnicity, 
-                         function(x){mean(x, na.rm=TRUE)}, 
-                         value.var = "Median_Household_Income_Dollars")
-
 census_combined$Geography_Type <- rename.levels(census_combined$Geography_Type,
                                                 orig = c("TA", "Region"),
                                                 ne = c("Territorial Authority", "Regional Council"))
+
+
 
 ethnicities <- names(census_combined)[-(1:3)]
 
